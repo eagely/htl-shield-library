cb292134c380497709dbf5d613fcd3d301e8d36c
eagely <77096372+eagely@users.noreply.github.com>
2022-11-25 18:26:44 +0100

    make adc functions and start on timer interrupt

--- include/universal.h
+++ include/universal.h
@@ -1,3 +1,12 @@
 namespace adc {
-    void init(byte pin);
+    void init(uint8_t pin, bool refs0 = false, bool refs1 = false, bool ate = false, uint16_t prescaler = 0);
+    void setPin(uint8_t pin);
+    void setReference(bool refs0, bool refs1);
+    void setAutoTrigger(bool ate);
+    void setPrescaler(uint16_t prescaler);
+    uint16_t getResult(uint8_t precision);
+}
+
+namespace timer {
+    void init(bool ovf = false, uint16_t prescaler = 0);
 }
--- src/universal.cpp
+++ src/universal.cpp
@@ -2,20 +2,43 @@
 #include <stdio.h>
 #include <math.h>
 #include "../include/universal.h"
-
+// Lord forgive me for what I am about to code
 namespace adc {
-    enum reference {};
-    void init(uint8_t pin, bool refs0 = false, bool refs1 = false, bool ate = false, uint16_t prescaler = 0) {
+    void init(uint8_t pin, bool refs0, bool refs1, bool ate, uint16_t prescaler) {
         ADMUX = pin | refs1 << REFS1 | refs0 << REFS0;
-        ADCSRA = 1 << ADEN | 1 << ADSC | ate << ADATE | prescaler;
+        ADCSRA = 1 << ADEN | 1 << ADSC | ate << ADATE | (int) (log10(prescaler) / log10(2));
     }
 
-    void setPin() {}
-    void setReference() {}
-    void setAutoTrigger() {}
-    void setPrescaler() {}
+    void setPin(uint8_t pin) {
+        ADMUX &= B11100000;
+        ADMUX |= pin;
+    }
 
-    void getResult(uint8_t precision = 10) {}
+    void setReference(bool refs0, bool refs1) {
+        ADMUX &= ~(1 << REFS0 | 1 << REFS1);
+        ADMUX |= refs0 << REFS0 | refs1 << REFS1;
+    }
+
+    void setAutoTrigger(bool ate) {
+        ADMUX &= ~(1 << ADATE);
+        ADMUX |= 1 << ADATE;
+    }
+
+    void setPrescaler(uint16_t prescaler) {
+        ADMUX &= ~(1 << ADPS0 | 1 << ADPS1 | 1 << ADPS2);
+        ADMUX |= (int) (log10(prescaler)/ log10(2));
+    }
+
+    uint16_t getResult() {
+        return ADCL | ADCH << 8;
+    }
+}
+// currently only supports overflow and prescaler
+namespace timer {
+    void init(bool ovf, uint16_t prescaler) {
+        TCCR1A =
+        TIMSK1 = ovf << TOIE1;
+    }
 }
 
 
