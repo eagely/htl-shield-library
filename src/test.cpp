5c7ea757f35c3e7edcc02411226ca1534a7c7dba
eagely <77096372+eagely@users.noreply.github.com>
2022-11-25 11:24:38 +0100

    added sample code

---
+++ src/test.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <stdio.h>
+#include "../include/test.h"
+
+void hello() {
+	digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void nothello() {
+    digitalWrite(LED_BUILTIN, LOW);
+}
+
+
+
+
+
+
+
+
